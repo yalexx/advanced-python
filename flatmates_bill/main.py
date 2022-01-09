f8bf9097f74d508b98f9caf2a2b07b4df47013de
Yanko Atanasov Aleksandrov <superactro@gmail.com>
2022-01-09 09:20:58 +0200

    oop bill

--- flatmates_bill/main.py
+++ flatmates_bill/main.py
@@ -1,16 +1,23 @@
+import json
+
+
 class Bill:
+    """
+    Object that contains data about a bill,
+    such as total amount and period of the bill.
+    """
     def __init__(self, amount, period):
-        self.amount = amount
         self.period = period
+        self.amount = amount
 
 
 class Flatmate:
     def __init__(self, name, days_in_house):
-        self.name = name
         self.days_in_house = days_in_house
+        self.name = name
 
-    def pays(self):
-        print('pays')
+    def pays(self, bill):
+        return bill.amount / 2
 
 
 class PdfReport:
@@ -18,4 +25,10 @@ class PdfReport:
         self.filename = filename
 
     def generate(self):
-        print('generate')
+        pass
+
+
+the_bill = Bill(amount=120, period = "March 2021")
+john = Flatmate(name="John", days_in_house=20)
+marry = Flatmate(name="Marry", days_in_house=10)
+print(john.pays(bill=the_bill))
