e42707412bc5d98987057b23aa8b7b821855cf3e
Yanko Atanasov Aleksandrov <superactro@gmail.com>
2022-01-14 11:21:26 +0200

    photo searcher

---
+++ photo-searcher/main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+Builder.load_file('frontend.kv')
+
+
+class FirstScreen(Screen):
+    def search_image(self):
+        pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+MainApp().run()
